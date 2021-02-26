bc022ce65dcbd7912f15993152508fd7f9b2cf5a
Chris Lawson <git@chris2fourlaw.me>
2021-02-25 17:20:40 -0800

    Add App test back in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
-  Header, Footer, Home,
+  Header, Footer, Landing,
 } from './components';
 
 const App = () => (
