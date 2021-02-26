0838a932a1c0905129c9ef534342d33d45030eca
Chris Lawson <git@chris2fourlaw.me>
2021-02-25 17:53:02 -0800

    Add login page placeholder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
-  Header, Footer, Landing,
+  Header, Footer, Landing, Login,
 } from './components';
 
 const App = () => (
@@ -11,7 +11,7 @@ const App = () => (
       <main role="main" className="inner content">
         <Switch>
           <Route path="/" exact component={() => <Landing />} />
-          {/* <Route path="/login" exact component={() => <Login />} /> */}
+          <Route path="/login" exact component={() => <Login />} />
         </Switch>
       </main>
       <Footer />
---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Login = () => (
+  <div>
+    <h1 className="login-heading">Login page.</h1>
+    <p className="lead">This is the Login page.</p>
+  </div>
+);
+
+export default Login;
