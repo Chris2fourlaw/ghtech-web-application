f92d31ebe2d061fbf008eeca6b0be5af17b18852
Chris Lawson <github@chris2fourlaw.me>
2021-02-25 22:47:04 -0800

    Add form to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const Login = () => (
   <div>
-    <h1 className="login-heading">Login page.</h1>
-    <p className="lead">This is the Login page.</p>
+    <h1 className="login-heading">Please sign in.</h1>
+    <Form className="form-signin">
+      <Form.Group controlId="formBasicEmail">
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group controlId="formBasicPassword">
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" className="checkbox mb-3" label="Remember me" />
+      </Form.Group>
+      <Button variant="primary" size="lg" className="btn-block" type="submit">Sign in</Button>
+    </Form>
   </div>
 );
 
